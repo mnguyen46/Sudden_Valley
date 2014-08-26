b8d4657c2633a7083328ac4f7facc66e35e48f6c
Nils <imperssonator@gmail.com>
2014-08-26 19:39:55 -0400

    No more Cells
    Each entry in the struct is now either a string or a number or a NaN,
    no more cell arrays

--- OFET_build.m
+++ OFET_build.m
@@ -8,6 +8,7 @@ OFET = struct();
 for i = 3:71
     for j = 1:28
         cat = alldata(j,2);
-        OFET(i).(cat{1})=alldata(j,i);
+        cellji = alldata(j,i);
+        OFET(i-2).(cat{1})=cellji{1};
     end
 end
