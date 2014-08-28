95b6c6bb8787c7b706d18ae8b3518619397ac13b
Nils <imperssonator@gmail.com>
2014-08-28 11:22:19 -0400

    Full Table
    All coating processes are now worked into the structure.

--- OFET_build.m
+++ OFET_build.m
@@ -3,7 +3,6 @@ clc
 
 [ndata, text, alldata] = xlsread('OFET fab table copy.xlsx');
 
-[m,n] = size(alldata);
 OFET = struct();
 for i = 3:71
     for j = 1:28
@@ -11,4 +10,24 @@ for i = 3:71
         cellji = alldata(j,i);
         OFET(i-2).(cat{1})=cellji{1};
     end
+    OFET(i-2).CoatProc='Spun';
+end
+
+[m,n] = size(OFET);
+for i = 3:8
+    for j = 32:58
+        cat = alldata(j,2);
+        cellji = alldata(j,i);
+        OFET(i-2+n).(cat{1})=cellji{1};
+    end
+    OFET(i-2+n).CoatProc='Dipped';
+end
+[m,n]=size(OFET);
+for i = 3:19
+    for j = 62:88
+        cat = alldata(j,2);
+        cellji = alldata(j,i);
+        OFET(i-2+n).(cat{1})=cellji{1};
+    end
+    OFET(i-2+n).CoatProc='Dropped';
 end
